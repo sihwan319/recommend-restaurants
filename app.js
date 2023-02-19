51826c867b23a14fc1d0f240452a71c50365560b
sihwankim <sihwan319@gmail.com>
2023-02-19 19:30:00 +0900

    node.js http req, res

--- app.js
+++ app.js
@@ -3,15 +3,41 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
 app.get('/', function(req, res){
+    const htmlFilePath = path.join(__dirname, 'views', 'index.html');
+    res.sendFile(htmlFilePath);
 
-    res.send('<h1>Hello World!!!!</h1>');
+    // res.send('<h1>Hello World!!!!</h1>');
 
-})
+});
 
 app.get('/restaurants', function(req, res){
     const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
     res.sendFile(htmlFilePath);
 });
+
+app.get('/recommend', function(req, res){
+    const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
+    res.sendFile(htmlFilePath);
+});
+
+app.post('/recommend', function(req, res) {
+    const retaurantsName = req.body.name;
+});
+
+app.get('/confirm', function(req, res){
+    const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
+    res.sendFile(htmlFilePath);
+});
+
+app.get('/about', function(req, res){
+    const htmlFilePath = path.join(__dirname, 'views', 'about.html');
+    res.sendFile(htmlFilePath);
+});
+
+
 
 app.listen(3000);
