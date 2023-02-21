1f6b14a69d0c7d782a21cc2add968ea6e5c521bf
sihwankim <sihwan319@gmail.com>
2023-02-21 09:02:47 +0900

    setting dynamic route

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const path = require('path');
-const fs = require('fs');
 
 const express = require('express');
+
+
+const defaultRoutes = require('./routes/default');
+const restaurantRoutes = require('./routes/restaurants');
+
 const app = express();
 
 app.set('views', path.join(__dirname,'views'));
@@ -10,48 +14,17 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', function(req, res){
-    res.render('index');
-
-});
-
-app.get('/restaurants', function(req, res){
-    const filePath = path.join(__dirname, 'data', 'restaurants.json');
-
-    const fileData = fs.readFileSync(filePath);
-    const storedRestaurants = JSON.parse(fileData);
-
-    res.render('restaurants', { numberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants});
-});
-
-app.get('/recommend', function(req, res){
-    // const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
-    // res.sendFile(htmlFilePath);
-    res.render('recommend');
-});
-
-app.post('/recommend', function(req, res) {
-    const restaurant = req.body;
-    const filePath = path.join(__dirname, 'data', 'restaurants.json');
-
-    const fileData = fs.readFileSync(filePath);
-    const storedRestaurants = JSON.parse(fileData);
-
-    storedRestaurants.push(restaurant);
-
-    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
-    
-    res.redirect('/confirm');
-});
-
-app.get('/confirm', function(req, res){
-    res.render('confirm');
-});
-
-app.get('/about', function(req, res){
-    res.render('about');
-});
+app.use('/', defaultRoutes);
+app.use('/', restaurantRoutes);
 
 
+
+app.use(function(req, res) { //다른 urlr경로에서 처리되지 않은(페이지를 찾지 못한 404) 요청이 처리될 것임.
+    res.status(404).render('404');
+})
+
+app.use(function(error, req, res, next) {
+    res.status(500).render('500');
+} )
 
 app.listen(3000);
